5e656d30afbca646c76e23044e43b3afd171a96d
Louis Laugesen <louis.laugesen@gmail.com>
2018-12-09 09:09:43 +1100

    Ignore fixable errors

--- linter.py
+++ linter.py
@@ -2,7 +2,7 @@ from SublimeLinter.lint import ComposerLinter
 
 class Phpcs(ComposerLinter):
     cmd = ('phpcs', '--report=csv', '${args}', '-')
-    regex = r'^(?!File).*",(?P<line>[0-9]+),(?P<col>[0-9]+),(?:(?P<error>error)|(?P<warning>warning)),(?P<message>".+",.+),[0-9],[01]'
+    regex = r'^(?!File).*",(?P<line>[0-9]+),(?P<col>[0-9]+),(?:(?P<error>error)|(?P<warning>warning)),(?P<message>".+",.+),[0-9],0'
     composer_name = 'phpcs'
     defaults = {
         'selector': 'source.php - text.blade, text.html.basic',
